caed70760392e779a2a6513d457f68eaf3ba6831
Bernt Hustad Hembre <bernt@clampon.no>
2016-03-28 23:34:49 +0200

    added SDcard file write

--- Diffvario.ino
+++ Diffvario.ino
@@ -1,16 +1,42 @@
 /**
-@brief Glider vario addon - diff sign. 
-@author Bernt Hustad Hembre
+\brief Glider vario addon - diff sign. 
+\author Bernt Hustad Hembre
 
+\todo: extract / rearrange SD-card writing to a seperate file/class
+\todo pwrdown Pin logic and definition update vs schematics.
 
 **/
 
-static char gpsstr[200];
+/*
+ The SD card circuit:
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 10
+ */
+
+#include <SPI.h>
+#include <SD.h>
+#include "parser.h"
+
+#define GPS_STR_LENGTH 100
+
+const int chipSelect = 10;
+static char gpsstr[GPS_STR_LENGTH];
 static int i;
+static File dataFile;
+static int maxstr_error = 0; // Counter for string overflow "exeptions"
+const int pwrdwnPin = 3;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+ 
+  if (!SD.begin(chipSelect)) {
+    return; // Nothing to do if SD fails.
+  }
+  dataFile = SD.open("datalog.txt", FILE_WRITE); /// \todo replace with function for unique filename
 }
 
 void loop() {
@@ -23,15 +49,26 @@ void loop() {
   
   // Check I2C and update if needed
   process_I2C();
+  
+  if(digitalRead(pwrdwnPin) == LOW){
+    dataFile.println("END");
+    dataFile.close();
+  }
+
 }
 
 
 void process_serial(void)
 {
   gpsstr[i++] = Serial.read();
-  while(Serial.available() && (gpsstr[i-1] != NULL))
+  while(Serial.available() && (gpsstr[i-1] != 0)){
     gpsstr[i++] = Serial.read();
-  if(gpsstr[i-1] == NULL){
+    if(i>=GPS_STR_LENGTH){
+      gpsstr[i-1] = 0; // End of buffer, terminating the string.  
+      maxstr_error++;
+    }
+  }
+  if(gpsstr[i-1] == 0){
     process_gpsstr(gpsstr);
     i=0;
   }
@@ -50,9 +87,27 @@ void process_I2C(){
 }
 
 void process_gpsstr(char *str){
-
+  char *Substr[15];
   
+  int errorcode = parsecsv( str, ',', Substr, 15, GPS_STR_LENGTH);
+  if(errorcode){
+    dataFile.print("Parsing Error code: ");
+    dataFile.print(String(errorcode));
+    dataFile.print(" String: \"");
+    dataFile.print(str);
+  }
+
+  // Example struture;
+  if(strcmp(Substr[0],"$GPSXXX"))
+    process_gpsxxx(Substr);
+  //dataFile.println(str);
   
 }
 
+void process_gpsxxx(char **sstr){
+  dataFile.println("GPS Dummy example");
+
+
+  
+}
 
